e96b805495061a6995cb097fbe49935f6a1f8226
F1rell <ratmir777mustafin@mail.ru>
2022-01-02 02:36:21 +0700

    Calc

--- CreditPaymentService.java
+++ CreditPaymentService.java
@@ -1,12 +1,12 @@
 public class CreditPaymentService {
     public double calculate(double credit, double percent, double duration) {
-        double percentMonths = percent / 100 / 12;
-        double qwe = 1 + percentMonths;
-        double qre =-duration;
-        double stepen = Math.pow(qwe , qre);
-        double sum = 1 - stepen;
-        double delenie = percentMonths / sum;
-        double payment = credit * delenie;
+        double percentMonths = percent / 100 / 12; // процент в месяц
+        double qwe = 1 + percentMonths; // расчёты по формуле
+        double qre =-duration; // перевод в отрицательное значение для дальнейшего корректного расчёта по формуле
+        double stepen = Math.pow(qwe , qre); // возведение в степень - степень это срок кредита
+        double sum = 1 - stepen; // расчёты по формуле
+        double delenie = percentMonths / sum; // расчёты по формуле
+        double payment = credit * delenie; // расчёты по формуле
         return payment;
     }
 }
